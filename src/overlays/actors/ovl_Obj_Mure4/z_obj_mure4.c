3e27ad6ef76a058562e4ab9765d5fd05527a8ae3
Yanis42 <yalink88@gmail.com>
2021-08-05 02:48:20 +0200

    Updated Obj_Mure4 to use every rotation values, added both option & iteration number, renamed and added functions

--- src/overlays/actors/ovl_Obj_Mure4/z_obj_mure4.c
+++ src/overlays/actors/ovl_Obj_Mure4/z_obj_mure4.c
@@ -4,6 +4,14 @@
  * Description: Tree + bush/rock spawner
  */
 
+// Actor parameters:
+// 0x000F: type/which type come first
+// 0x00F0: 1 for both types
+// 0x0F00: for variant number
+// 0xF000: for repetition number
+
+//TODO: add custom distance and direction parameters
+
 #include "z_obj_mure4.h"
 
 #define THIS ((ObjMure4*)thisx)
@@ -11,9 +19,13 @@
 void ObjMure4_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure4_Destroy(Actor* thisx, GlobalContext* globalCtx);
 void ObjMure4_Update(Actor* thisx, GlobalContext* globalCtx);
-void ObjMure4_ActorSpawn(ObjMure4* this, GlobalContext* globalCtx);
-s16 ObjMure4_SetWood02Vars(ObjMure4* this, Vec3f pos);
-s16 ObjMure4_SetVariantVars(ObjMure4* this, Vec3f pos, Vec3s rot);
+void ObjMure4_ActorSpawn(ObjMure4* this, GlobalContext* globalCtx, Vec3f pos, Vec3f pos2, Vec3s rot);
+void ObjMure4_SpawnTree(GlobalContext *globalCtx, Vec3f pos, Vec3s rot);
+void ObjMure4_SpawnVariant(GlobalContext *globalCtx, Vec3f pos);
+void ObjMure4_SetVariantParams(ObjMure4* this, Vec3s rot, s16 params);
+void ObjMure4_SpawnBoth(ObjMure4* this, GlobalContext *globalCtx, Vec3f pos, Vec3s rot, s16 params);
+s16 ObjMure4_GetWood02Vars(Vec3s rot);
+s16 ObjMure4_GetVariantVars(ObjMure4* this, Vec3s rot);
 
 const ActorInit Obj_Mure4_InitVars = {
     ACTOR_OBJ_MURE4,
@@ -28,63 +40,92 @@ const ActorInit Obj_Mure4_InitVars = {
 };
 
 static s16 sActorSpawnIDs[] = { ACTOR_EN_WOOD02, ACTOR_EN_KUSA, ACTOR_EN_ISHI };
-static s16 type = 0xFFFF;
+static s16 type = 0xFFFE, spawnParams = 0xFFFE;
 
 void ObjMure4_Init(Actor* thisx, GlobalContext* globalCtx){
     ObjMure4* this = THIS;
 
-    ObjMure4_ActorSpawn(this, globalCtx);
+    type = this->actor.params & 0xF;
+    ObjMure4_ActorSpawn(this, globalCtx, this->actor.world.pos, this->actor.world.pos, this->actor.world.rot);
 }
 
-void ObjMure4_ActorSpawn(ObjMure4* this, GlobalContext* globalCtx){
-    Vec3f pos;
-    Vec3s rot;
-    s16 i, params;
+//main function that spawns the actors, still WIP
+void ObjMure4_ActorSpawn(ObjMure4* this, GlobalContext* globalCtx, Vec3f pos, Vec3f pos2, Vec3s rot){
+    s16 i, j, params, variantNb, bitBoth, howMany;
 
-    pos = this->actor.world.pos;
-    rot.z = this->actor.world.rot.z;
-    //0xF000 for switch flag, 0x0F00 for number of times, 0x00F0 for drops, 0x000F for bugs
-    //params: 0xF000 for wood02 drops, 0x0F00 for wood02 type, 0x00F0 for type of type of actor, 0x000F type of actor
+    params = this->actor.params;
+    variantNb = (params >> 8) & 0xF;
+    bitBoth = (params & 0xFF) >> 4;
+    howMany = params >> 12;
+    ObjMure4_SetVariantParams(this, rot, params);
+    pos2.x = this->actor.world.pos.x + 100;
+    
+    ObjMure4_SpawnTree(globalCtx, pos, rot);
+    //place the first tree
 
-    for(i = 0; i < 5; i++){
-        if(i == 0){
-            params = ObjMure4_SetWood02Vars(this, pos);
+    for(j = 0; j < howMany; j++){
+        for(i = 0; i < variantNb; i++){
+            if(!bitBoth) ObjMure4_SpawnVariant(globalCtx, pos2);
+            else ObjMure4_SpawnBoth(this, globalCtx, pos2, rot, params);
+            pos2.x += 100;
+            //place the variants and increment pos2.x by the custom distance from the mure4 params/rotation
         }
-        else {
-            params = ObjMure4_SetVariantVars(this, pos, rot);
-        }
-
-        Actor_Spawn(&globalCtx->actorCtx, globalCtx, sActorSpawnIDs[type],
-                                                 (pos.x + (i * 100.f)), pos.y, pos.z,
-                                                 0, 0, 0, params);
         
+        pos.x = pos2.x;
+        pos2.x += 100;
+        ObjMure4_SpawnTree(globalCtx, pos, rot);
+
+        if(bitBoth){
+            if(type == 1) type = 2;
+            else type = 1;
+        }
+        //place the last tree of the current iteration and switch type if needed
     }
 }
 
-s16 ObjMure4_SetWood02Vars(ObjMure4* this, Vec3f pos){
-    s16 params = 0xFFFF;
-
-    params = this->actor.params;
-    type = 0;
-    return ((((params >> 8) & 0xF0) << 4) + ((params >> 8) & 0xF));
-    //item drop, type
+//used to spawn both variants, alternating
+void ObjMure4_SpawnBoth(ObjMure4* this, GlobalContext *globalCtx, Vec3f pos, Vec3s rot, s16 params){
+    ObjMure4_SetVariantParams(this, rot, params);
+    ObjMure4_SpawnVariant(globalCtx, pos);
+    
+    if(type == 1) type = 2;
+    else type = 1;
 }
 
-s16 ObjMure4_SetVariantVars(ObjMure4* this, Vec3f pos, Vec3s rot){
-    s16 params = 0xFFFF;
+//get En_Wood02 parameters from the Mure4 X rot
+s16 ObjMure4_GetWood02Vars(Vec3s rot){
+    return rot.x;
+}
 
-    params = this->actor.params;
-    type = params & 0xF;
-
+//get the variant's parameters from the Mure4 Y or Z rot
+s16 ObjMure4_GetVariantVars(ObjMure4* this, Vec3s rot){
     switch(type){
         case 1: //bush
-            return ((((rot.z & 0xFF) >> 4) << 8) + ((rot.z & 0xF) << 4) + ((params & 0xFF) >> 4));
-            //drop table, bugs, type
-
+            return rot.y;
+            
         case 2: //rock
-            return (((rot.z >> 12) << 12) + (((rot.z & 0xFF) >> 4) << 8) + ((rot.z & 0xF) << 4) + ((params & 0xFF) >> 4));
-            //switch flag, drop table, bugs, type
+            return rot.z;
     } 
+}
+
+//spawn a single tree
+void ObjMure4_SpawnTree(GlobalContext *globalCtx, Vec3f pos, Vec3s rot){
+    Actor_Spawn(&globalCtx->actorCtx, globalCtx, sActorSpawnIDs[0], pos.x, pos.y, pos.z, 0, 0, 0, ObjMure4_GetWood02Vars(rot));
+}
+
+//spawn a single variant
+void ObjMure4_SpawnVariant(GlobalContext *globalCtx, Vec3f pos){
+    Actor_Spawn(&globalCtx->actorCtx, globalCtx, sActorSpawnIDs[type], pos.x, pos.y, pos.z, 0, 0, 0, spawnParams);
+}
+
+//set variant's parameters
+void ObjMure4_SetVariantParams(ObjMure4* this, Vec3s rot, s16 params){
+    if(!((params & 0xFF) >> 4)){
+        if(params & 0xF) type = 1;
+        else type = 2;
+    } 
+
+    spawnParams = ObjMure4_GetVariantVars(this, rot);
 }
 
 void ObjMure4_Destroy(Actor* thisx, GlobalContext* globalCtx){
