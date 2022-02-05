7992177d0b42b22b2cb4a8bea3be30ab83d659d6
Yanis42 <yalink88@gmail.com>
2022-02-05 19:14:47 +0100

    z_opening

--- src/overlays/gamestates/ovl_opening/z_opening.c
+++ src/overlays/gamestates/ovl_opening/z_opening.c
@@ -7,12 +7,36 @@
 #include "global.h"
 
 void Opening_SetupTitleScreen(OpeningContext* this) {
-    gSaveContext.gameMode = 1;
-    this->state.running = false;
-    gSaveContext.linkAge = LINK_AGE_ADULT;
+    // gSaveContext.gameMode = 1;
+    // this->state.running = false;
+    // gSaveContext.linkAge = LINK_AGE_ADULT;
+    // Sram_InitDebugSave();
+    // gSaveContext.cutsceneIndex = 0xFFF3;
+    // gSaveContext.sceneSetupIndex = 7;
+    // SET_NEXT_GAMESTATE(&this->state, Gameplay_Init, GlobalContext);
+
     Sram_InitDebugSave();
-    gSaveContext.cutsceneIndex = 0xFFF3;
-    gSaveContext.sceneSetupIndex = 7;
+    gSaveContext.gameMode = 0;
+    gSaveContext.unk_13F6 = gSaveContext.magic;
+    gSaveContext.magic = 0;
+    gSaveContext.unk_13F4 = 0;
+    gSaveContext.magicLevel = gSaveContext.magic;
+
+    gSaveContext.linkAge = LINK_AGE_CHILD;
+    gSaveContext.dayTime = 0x8000;
+    gSaveContext.buttonStatus[4] = BTN_ENABLED;
+    gSaveContext.buttonStatus[3] = BTN_ENABLED;
+    gSaveContext.buttonStatus[2] = BTN_ENABLED;
+    gSaveContext.buttonStatus[1] = BTN_ENABLED;
+    gSaveContext.buttonStatus[0] = BTN_ENABLED;
+    gSaveContext.unk_13E7 = gSaveContext.unk_13E8 = gSaveContext.unk_13EA = gSaveContext.unk_13EC = 0;
+    Audio_QueueSeqCmd(NA_BGM_STOP);
+    gSaveContext.entranceIndex = 0x33; //B1 for mkt, 171 for tot exterior, 33 for mkt entrance, 1FD for field, 82 for spirit
+    gSaveContext.respawnFlag = 0;
+    gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = -1;
+    gSaveContext.showTitleCard = true;
+    gWeatherMode = 0;
+    this->state.running = false;
     SET_NEXT_GAMESTATE(&this->state, Gameplay_Init, GlobalContext);
 }
 
