e0def49c6945e327aa3b409ca312b3991025cc9a
Yanis42 <yalink88@gmail.com>
2022-05-12 15:28:01 +0200

    entrance test

--- src/overlays/gamestates/ovl_opening/z_opening.c
+++ src/overlays/gamestates/ovl_opening/z_opening.c
@@ -32,11 +32,9 @@ void Opening_SetupTitleScreen(OpeningContext* this) {
     gSaveContext.buttonStatus[0] = BTN_ENABLED;
     gSaveContext.unk_13E7 = gSaveContext.unk_13E8 = gSaveContext.unk_13EA = gSaveContext.unk_13EC = 0;
     Audio_QueueSeqCmd(NA_BGM_STOP);
-    gSaveContext.entranceIndex = 0x33; //B1 for mkt, 171 for tot exterior, 33 for mkt entrance, 1FD for field, 82 for spirit
+    gSaveContext.entranceIndex = ENTR_DETAILED_PANEL_0_CD;
     gSaveContext.respawnFlag = 0;
     gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = -1;
-    // gSaveContext.seqIndex = 0xFF;
-    // gSaveContext.nightSeqIndex = 0xFF;
     gSaveContext.showTitleCard = true;
     gWeatherMode = 0;
     this->state.running = false;
