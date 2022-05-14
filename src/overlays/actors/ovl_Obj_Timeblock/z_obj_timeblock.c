8045428f2002d93dbc116c53e88abd832a0be73b
Yanis42 <yalink88@gmail.com>
2022-05-14 22:58:44 +0200

    Improved timeblock alt behavior

--- src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
+++ src/overlays/actors/ovl_Obj_Timeblock/z_obj_timeblock.c
@@ -8,6 +8,7 @@
 #include "objects/object_timeblock/object_timeblock.h"
 
 #define FLAGS (ACTOR_FLAG_0 | ACTOR_FLAG_4 | ACTOR_FLAG_25 | ACTOR_FLAG_27)
+#define SWITCH(params) (params & 0x3F)
 
 void ObjTimeblock_Init(Actor* thisx, GlobalContext* globalCtx);
 void ObjTimeblock_Destroy(Actor* thisx, GlobalContext* globalCtx);
@@ -125,9 +126,12 @@ void ObjTimeblock_Init(Actor* thisx, GlobalContext* globalCtx) {
     this->unk_175 = ((this->dyna.actor.params >> 15) & 1) ? true : false;
     this->isVisible = ObjTimeblock_CalculateIsVisible(this);
 
+    osSyncPrintf("isVisible: %d\n", this->isVisible);
+
     if (!((this->dyna.actor.params >> 10) & 1)) {
         ObjTimeblock_SetupNormal(this);
     } else if (this->isVisible) {
+        this->unk_176 = this->unk_174;
         ObjTimeblock_SetupAltBehaviorVisible(this);
     } else {
         ObjTimeblock_SetupAltBehaviourNotVisible(this);
@@ -270,14 +274,16 @@ void ObjTimeblock_SetupAltBehaviorVisible(ObjTimeblock* this) {
 }
 
 void ObjTimeblock_AltBehaviorVisible(ObjTimeblock* this, GlobalContext* globalCtx) {
-    if (this->songObserverFunc(this, globalCtx) && this->demoEffectTimer <= 0) {
+    u8 songMode = this->songObserverFunc(this, globalCtx);
+    u8 flagMode = !this->unk_176 && this->isVisible ? true : false;
+    if ((songMode || flagMode) && this->demoEffectTimer <= 0) {
         this->demoEffectFirstPartTimer = 12;
         ObjTimeblock_SpawnDemoEffect(this, globalCtx);
         this->demoEffectTimer = 160;
         OnePointCutscene_Attention(globalCtx, &this->dyna.actor);
         // "Time Block Attention Camera (frame counter)"
         osSyncPrintf("◯◯◯◯ Time Block 注目カメラ (frame counter  %d)\n", globalCtx->state.frames);
-        ObjTimeblock_ToggleSwitchFlag(globalCtx, this->dyna.actor.params & 0x3F);
+        Flags_UnsetSwitch(globalCtx, SWITCH(this->dyna.actor.params));
     }
 
     func_80BA06AC(this, globalCtx);
